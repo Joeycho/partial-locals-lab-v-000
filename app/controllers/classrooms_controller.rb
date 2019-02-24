03d0797d283e43c821173a1ce64fb6913df87f3e
Joey Cho <cjy12182000@naver.com>
2019-02-24 05:56:38 +0000

    Automatically backed up by Learn

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -2,6 +2,11 @@ class ClassroomsController < ApplicationController
   def show
     binding.pry
     @classroom = Classroom.find(params[:id])
+    if @classroom
+      
+    else
+      errors.add(:classroom, "No such a classroom here")
+    end
   end
 
   def index
